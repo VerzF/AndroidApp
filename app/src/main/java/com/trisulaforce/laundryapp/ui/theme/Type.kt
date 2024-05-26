99f1fcbf0cdb7b73b0dce9fcc3c363aa20cfb102
Aldio Yohanes <aldioguire@gmail.com>
2024-05-27 00:12:36 +0700

    adding changes from aldio

--- app/src/main/java/com/trisulaforce/laundryapp/ui/theme/Type.kt
+++ app/src/main/java/com/trisulaforce/laundryapp/ui/theme/Type.kt
@@ -2,9 +2,22 @@ package com.trisulaforce.laundryapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.trisulaforce.laundryapp.R
+
+val popinsFontFamily = FontFamily(
+    Font(R.font.poppins_black, FontWeight.Black),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_extrabold, FontWeight.ExtraBold),
+    Font(R.font.poppins_extralight, FontWeight.ExtraLight),
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_thin, FontWeight.Thin)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,6 +27,11 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = popinsFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
